8455db75e2707dcb5eaa2d66aee42b6a5c5a91b0
bdamirul0 <bdamirul0@gmail.com>
2020-03-13 11:11:36 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,20 @@
+// 0 (false)
+// "" (false)
+// NaN( false)
+//null(false);
+
+// " " (true);
+// "0"(true)
+//{}(true); As this is an object
+// [] (true) ; As this is an array
+
+
+
+const name = "Amirul Islam";
+
+if(name.length){
+     console.log("The age is true");
+}
+else{
+    console.log("The age is false");
+}
